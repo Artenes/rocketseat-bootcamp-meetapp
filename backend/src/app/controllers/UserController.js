069299f47bd1e6b83e009a281fac61dd6a4fdcfb
Artenes Nogueira <artenesama@gmail.com>
2019-08-28 20:52:42 -0400

    [backend] Adds validation to /users route

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,5 +1,12 @@
+import createUserSchema from '../schemas/CreateUserSchema';
+
 class UserController {
   async store(req, res) {
+    const isValid = await createUserSchema.isValid(req.body);
+    if (!isValid) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { name, email, password } = req.body;
 
     return res.json({
