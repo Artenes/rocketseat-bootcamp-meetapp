ae483a6a2ebb7c0da26aa24528837c10351bbc53
Artenes Nogueira <artenesama@gmail.com>
2019-08-28 21:03:25 -0400

    [backend] Creates new user in database

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,18 +1,24 @@
 import createUserSchema from '../schemas/CreateUserSchema';
+import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const isValid = await createUserSchema.isValid(req.body);
-    if (!isValid) {
+    const isSchemaValid = await createUserSchema.isValid(req.body);
+    if (!isSchemaValid) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
 
     return res.json({
+      id,
       name,
       email,
-      password,
     });
   }
 }
