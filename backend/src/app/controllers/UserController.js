5a4c5d01b6ae37fc984e25b9acb76f94c1da8ab3
Artenes Nogueira <artenesama@gmail.com>
2019-08-29 23:09:02 -0400

    [backend] Creates route to updates user data

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,4 +1,5 @@
 import createUserSchema from '../schemas/CreateUserSchema';
+import updateUserSchema from '../schemas/UpdateUserSchema';
 import User from '../models/User';
 
 class UserController {
@@ -14,6 +15,40 @@ class UserController {
     }
 
     const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    const isSchemaValid = await updateUserSchema.isValid(req.body);
+    if (!isSchemaValid) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword) {
+      const passwordValid = await user.checkPassword(oldPassword);
+      if (oldPassword && !passwordValid) {
+        return res.status(401).json({ error: 'Password does not match' });
+      }
+    }
+
+    const { id, name } = await user.update(req.body);
 
     return res.json({
       id,
