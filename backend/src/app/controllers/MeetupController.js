a651a0b573bfd4cbca471a427e679757e050a65f
Artenes Nogueira <artenesama@gmail.com>
2019-09-04 19:35:14 -0400

    Adds POST /meetups route

---
+++ backend/src/app/controllers/MeetupController.js
@@ -0,0 +1,7 @@
+class MeetupController {
+  async store(req, res) {
+    return res.json();
+  }
+}
+
+export default new MeetupController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 
 import multerConfig from './config/multer';
+import MeetupController from './app/controllers/MeetupController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,5 +21,7 @@ routes.use(authMiddleware);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.put('/users', UserController.update);
+
+routes.post('/meetups', MeetupController.store);
 
 export default routes;
