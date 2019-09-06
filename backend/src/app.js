ae572bb633053fadc70955edcb80cd219c051dd5
Artenes Nogueira <artenesama@gmail.com>
2019-09-06 11:52:31 -0400

    Throw 404 errors when static files are not found

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,16 @@ class App {
     this.server.use(express.json());
 
     // creates route to serve static files
-    this.server.use('/files', express.static(multerConfig.destination));
+    // when a file is not found, it will throw a 404 error
+    // instead of just passing down the request
+    // this option is not valid if we have multiple
+    // calls to express.static that sets multiples dirs
+    // to be looked up for static files.
+    const throwErrors = { fallthrough: false };
+    this.server.use(
+      '/files',
+      express.static(multerConfig.destination, throwErrors)
+    );
   }
 
   routes() {
