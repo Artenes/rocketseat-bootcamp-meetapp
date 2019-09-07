a7b574adf9e074208f519800bf1260d261d4d5eb
Artenes Nogueira <artenesama@gmail.com>
2019-09-07 15:14:30 -0400

    [backend] Allows users to see the meetups he/she took part

--- backend/src/app/controllers/RegistrationController.js
+++ backend/src/app/controllers/RegistrationController.js
@@ -1,14 +1,59 @@
+import { Op } from 'sequelize';
 import RegistrationStoreRequest from '../requests/RegistrationStoreRequest';
 import Registration from '../models/Registration';
 import User from '../models/User';
 import Meetup from '../models/Meetup';
 import Mail from '../../lib/Mail';
+import File from '../models/File';
 
 /**
  * Controller for managing registrations.
  * Registration is when a user decides to take part in a meetup.
  */
 class RegistrationController {
+  /**
+   * Lists all meetups the user took part.
+   *
+   * @param {Object} req the incoming request.
+   * @param {Object} res the outgoing response.
+   */
+  async index(req, res) {
+    const registrations = await Registration.findAll({
+      include: [
+        {
+          model: Meetup,
+          where: { date: { [Op.gt]: new Date() } },
+          required: true,
+          include: [{ model: File, as: 'banner' }],
+        },
+      ],
+      order: [[Meetup, 'date']],
+    });
+
+    // retarded solution just to make banner points to url
+    const meetups = registrations.map(registration => {
+      const {
+        id,
+        title,
+        description,
+        localization,
+        date,
+        banner,
+      } = registration.Meetup;
+
+      return {
+        id,
+        title,
+        description,
+        localization,
+        date,
+        banner: banner.url,
+      };
+    });
+
+    return res.json(meetups);
+  }
+
   /**
    * Creates a registration.
    *
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -30,6 +30,7 @@ routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
 
+routes.get('/registrations', RegistrationController.index);
 routes.post('/registrations', RegistrationController.store);
 
 export default routes;
