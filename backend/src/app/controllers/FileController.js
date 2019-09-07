7132655909aba1ea0eb3d54112eb76c7ca726a2e
Artenes Nogueira <artenesama@gmail.com>
2019-09-07 15:33:16 -0400

    Refactors /files route
    Put the request class in the new standard and remove some unecessary code from the controller.

--- backend/src/app/controllers/FileController.js
+++ backend/src/app/controllers/FileController.js
@@ -2,30 +2,36 @@ import fileSystem from 'fs';
 import FileStoreRequest from '../requests/FileStoreRequest';
 import File from '../models/File';
 
-function deleteFileIfPresent(req) {
-  if (req.file) {
-    try {
-      fileSystem.unlinkSync(req.file.path);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
-
+/**
+ * Controls the upload of files.
+ */
 class FileController {
+  /**
+   * Save a file's information in the database.
+   * And do any clean up in case the request is invalid.
+   *
+   * @param {Object} req the incoming request.
+   * @param {Object} res the outgoing response.
+   */
   async store(req, res) {
-    const { status, error } = FileStoreRequest.isValid(req);
+    const { status, error } = await new FileStoreRequest(req).isValid();
     if (error) {
       // if something went wrong, delete the uploaded file
-      deleteFileIfPresent(req);
+      if (req.file) {
+        try {
+          fileSystem.unlinkSync(req.file.path);
+        } catch (exception) {
+          // just ignore the error.
+        }
+      }
       return res.status(status).json({ error });
     }
 
     const { originalname: name, filename: path } = req.file;
 
-    const file = await File.create({ name, path });
+    await File.create({ name, path });
 
-    return res.json(file);
+    return res.sendStatus(201);
   }
 }
 
