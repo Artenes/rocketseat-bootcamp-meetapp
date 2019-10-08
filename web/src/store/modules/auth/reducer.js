e3b15ae8573e1fbf67cf591907d5c0997d2a3d14
Artenes Nogueira <artenesama@gmail.com>
2019-10-07 20:48:20 -0400

    Shows loading feedback when SignUp
    Just to keep consistency between signup and signin.

--- web/src/store/modules/auth/reducer.js
+++ web/src/store/modules/auth/reducer.js
@@ -12,6 +12,9 @@ export default function auth(state = INITIAL_STATE, action) {
       case '@auth/SIGN_IN_REQUEST':
         draft.loading = true;
         break;
+      case '@auth/SIGN_UP_REQUEST':
+        draft.loading = true;
+        break;
       case '@auth/SIGN_IN_SUCCESS':
         draft.token = action.payload.token;
         draft.signed = true;
