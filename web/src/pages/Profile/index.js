fe049eda8d4661d8fc535215f6ad39bf1ebf0768
Artenes Nogueira <artenesama@gmail.com>
2019-10-08 20:42:17 -0400

    [backend] Changes profile page layout
    Now the layout reflects the documentation.

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,51 +1,40 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import { MdAddCircleOutline } from 'react-icons/md';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
-import { signOut } from '~/store/modules/auth/actions';
 import { Container } from './styles';
-import AvatarInput from './AvatarInput';
 
 export default function Profile() {
   const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data, { resetForm }) {
-    // dispatch(updateProfileRequest(data));
-    resetForm({ name: 'dfgdgfdg' });
-  }
-
-  function handleSignOut() {
-    dispatch(signOut());
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
   }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
-        <AvatarInput name="avatar_id" />
-        <Input name="name" placeholder="Full name" />
-        <Input name="email" type="email" placeholder="Your e-mail address" />
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Digite seu e-mail" />
 
         <hr />
 
-        <Input
-          type="password"
-          name="oldPassword"
-          placeholder="Your current password"
-        />
+        <Input type="password" name="oldPassword" placeholder="Senha atual" />
 
-        <Input type="password" name="password" placeholder="New password" />
+        <Input type="password" name="password" placeholder="Nova senha" />
         <Input
           type="password"
           name="confirmPassword"
-          placeholder="Confirm your password"
+          placeholder="Confirmação de senha"
         />
-        <button type="submit">Update profile</button>
+        <button type="submit">
+          <MdAddCircleOutline size={24} color="#fff" />
+          Salvar perfil
+        </button>
       </Form>
-      <button type="button" onClick={handleSignOut}>
-        Log out
-      </button>
     </Container>
   );
 }
--- web/src/pages/Profile/styles.js
+++ web/src/pages/Profile/styles.js
@@ -4,25 +4,27 @@ import { darken } from 'polished';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 600px;
+  max-width: 940px;
   margin: 50px auto;
 
   form {
     display: flex;
     flex-direction: column;
-    margin-top: 30px;
+    margin-top: 50px;
 
     input {
-      background: rgba(0, 0, 0, 0.1);
+      background: rgba(0, 0, 0, 0.2);
       border: 0;
       border-radius: 4px;
-      height: 44px;
+      height: 50px;
       padding: 0 15px;
       color: #fff;
       margin: 0 0 10px;
+      font-size: 18px;
 
       &::placeholder {
-        color: rgba(255, 255, 255, 0.7);
+        color: rgba(255, 255, 255, 0.5);
+        font-size: 18px;
       }
     }
 
@@ -36,23 +38,33 @@ export const Container = styled.div`
     hr {
       border: 0;
       height: 1px;
-      background: rgba(255, 255, 255, 0.2);
-      margin: 10px 0 20px;
+      background: rgba(255, 255, 255, 0.1);
+      margin: 20px 0 19px;
     }
 
     button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 162px;
       margin: 5px 0 0;
       height: 44px;
-      background: #3b93ff;
+      background: #f94d6a;
       font-weight: bold;
       color: #fff;
       border: 0;
       border-radius: 4px;
       font-size: 16px;
       transition: background 0.2s;
+      margin-top: 10px;
+      margin-left: auto;
+
+      svg {
+        margin-right: 10px;
+      }
 
       &:hover {
-        background: ${darken(0.03, '#3b93ff')};
+        background: ${darken(0.03, '#F94D6A')};
       }
     }
 
@@ -65,22 +77,6 @@ export const Container = styled.div`
       &:hover {
         opacity: 1;
       }
-    }
-  }
-
-  > button {
-    margin: 10px 0 0;
-    height: 44px;
-    background: #f64c75;
-    font-weight: bold;
-    color: #fff;
-    border: 0;
-    border-radius: 4px;
-    font-size: 16px;
-    transition: background 0.2s;
-
-    &:hover {
-      background: ${darken(0.03, '#F64C75')};
     }
   }
 `;
